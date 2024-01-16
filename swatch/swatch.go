1773759b7536902a74760be94b2380b274237ea9
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-16 12:05:13 -0500

    feat: create Swatch Widget to set or change color

---
+++ swatch/swatch.go
@@ -0,0 +1,32 @@
+package swatch
+
+import (
+	"image/color"
+	"sixam/gopixel/apptype"
+
+	"fyne.io/fyne/v2/widget"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) SetColor(c color.Color) {
+	s.Color = c
+	s.Refresh()
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+	swatch := &Swatch{
+		Selected:     false,
+		Color:        color,
+		clickHandler: clickHandler,
+		SwatchIndex:  swatchIndex,
+	}
+	swatch.ExtendBaseWidget(swatch)
+	return swatch
+}
