0a20b8fe7c1c57c05b186f0e07a79a7967275d75
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-17 00:12:45 -0500

    feat: add renderer fns to refresh, layout img and draw onscreen borders

--- pixelcanvas/pixelcanvasrenderer.go
+++ pixelcanvas/pixelcanvasrenderer.go
@@ -20,6 +20,7 @@ func (renderer *PixelCanvasRenderer) MinSize() fyne.Size {
 func (renderer *PixelCanvasRenderer) Objects() []fyne.CanvasObject {
 	objects := make([]fyne.CanvasObject, 0, 5) // 4 out of 5 objects will be border lines
 
+	// First four border objects on LayoutBorder func
 	for i := 0; i < len(renderer.canvasBorder); i++ {
 		objects = append(objects, &renderer.canvasBorder[i])
 	}
@@ -30,8 +31,58 @@ func (renderer *PixelCanvasRenderer) Objects() []fyne.CanvasObject {
 
 func (renderer *PixelCanvasRenderer) Destroy() {}
 
-// Todo:
-// Layout fn
+// Base Layout, layout canvas needs to be called first
+func (renderer *PixelCanvasRenderer) Layout(size fyne.Size) {
+	renderer.LayoutCanvas(size)
+	renderer.LayoutBorder(size)
+}
+
+// Refresh - checks if we need to reload the image
+func (renderer *PixelCanvasRenderer) Refresh() {
+	if renderer.pixelCanvas.reloadImage {
+		renderer.canvasImage = renderer.NewImageFromImage(renderer.pixelCanvas.PixelData)
+		// change out image is scaled, smooth or pixel
+		renderer.canvasImage.ScaleMode = canvas.ImageScalePixels
+		renderer.canvasImage.FillMode = canvas.ImageFillContain
+		renderer.pixelCanvas.reloadImage = false
+	}
+	renderer.Layout(renderer.pixelCanvas.Size())
+	canvas.Refresh(renderer.canvasImage)
+}
+
 // Layout img, px width and height
-// Allow panning and zoom into pixels for ease of editing
-// Actual onscreen size of image
+func (renderer *PixelCanvasRenderer) LayoutCanvas(size fyne.Size) {
+	imagePixelWidth := renderer.pixelCanvas.PixelCols
+	imagePixelHeight := renderer.pixelCanvas.PixelRows
+	pixelSize := renderer.pixelCanvas.PixelSize
+
+	// Allow panning and zoom into pixels for ease of editing
+	renderer.canvasImage.Move(fyne.NewPos(renderer.PixelCanvas.CanvasOffset.X, renderer.pixelCanvas.CanvasOffset.Y))
+	renderer.canvasImage.Resize(fyne.NewSize(float32(imagePixelWidth*pixelSize), float32(imagePixelHeight*pixelSize)))
+}
+
+// Create borders of actual onscreen size of image
+func (renderer *PixelCanvasRenderer) LayoutBorder(size fyne.Size) {
+	offset := renderer.pixelCanvas.CanvasOffset
+	imageHeight := renderer.canvasImage.Size().Height
+	imageWidth := renderer.canvasImage.Size().Width
+
+	// Positions represent start and ending line positions
+	left := &renderer.canvasBorder[0]
+	left.Position1 = fyne.NewPos(offset.X, offset.Y)
+	left.Position2 = fyne.NewPos(offset.X, offset.Y+imageHeight)
+
+	top := &renderer.canvasBorder[1]
+	top.Position1 = fyne.NewPos(offset.X, offset.Y)
+	top.Position2 = fyne.NewPos(offset.X+imageWidth, offset.Y)
+
+	right := &renderer.canvasBorder[2]
+	right.Position1 = fyne.NewPos(offset.X+imageWidth, offset.Y)
+	right.Position2 = fyne.NewPos(offset.X+imageWidth, offset.Y+imageHeight)
+
+	bottom := &renderer.canvasBorder[3]
+	bottom.Position1 = fyne.NewPos(offset.X+imageWidth, offset.Y)
+	bottom.Position2 = fyne.NewPos(offset.X+imageWidth, offset.Y+imageHeight)
+}
+
+func (renderer *PixelCanvasRenderer) Layout(size fyne.Size) {}
